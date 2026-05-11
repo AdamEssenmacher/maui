f4b19b6d5844e527b2f16cdc21acd801e5b4bffb
Adam Essenmacher <adam.essenmacher@gmail.com>
2026-05-11 15:21:22 -0400

    [iOS] Fix Switch custom colors on iOS 26

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue35257.cs
@@ -0,0 +1,43 @@
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 35257, "Switch custom colors render on iOS 26", PlatformAffected.iOS)]
+public class Issue35257 : ContentPage
+{
+	public Issue35257()
+	{
+		Content = new VerticalStackLayout
+		{
+			Padding = new Thickness(30),
+			Spacing = 25,
+			Children =
+			{
+				new Switch
+				{
+					AutomationId = "CustomOffSwitch",
+					IsToggled = false,
+					OffColor = Colors.Red,
+					OnColor = Colors.Green,
+					ThumbColor = Colors.Orange
+				},
+				new Switch
+				{
+					AutomationId = "DefaultOffSwitch",
+					IsToggled = false
+				},
+				new Switch
+				{
+					AutomationId = "CustomOnSwitch",
+					IsToggled = true,
+					OffColor = Colors.Red,
+					OnColor = Colors.Green,
+					ThumbColor = Colors.Orange
+				},
+				new Switch
+				{
+					AutomationId = "DefaultOnSwitch",
+					IsToggled = true
+				}
+			}
+		};
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue35257.cs
@@ -0,0 +1,39 @@
+#if IOS
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue35257 : _IssuesUITest
+{
+	public override string Issue => "Switch custom colors render on iOS 26";
+
+	public Issue35257(TestDevice device) : base(device)
+	{
+	}
+
+	public override IConfig GetTestConfig()
+	{
+		var config = base.GetTestConfig();
+		config.SetTestConfigurationArg("test", Issue);
+		return config;
+	}
+
+	protected override void TryToResetTestState()
+	{
+	}
+
+	[Test]
+	[Category(UITestCategories.Switch)]
+	public void SwitchCustomColorsRenderOnInitialState()
+	{
+		App.WaitForElement("CustomOffSwitch", timeout: TimeSpan.FromSeconds(60));
+		App.WaitForElement("DefaultOffSwitch");
+		App.WaitForElement("CustomOnSwitch");
+		App.WaitForElement("DefaultOnSwitch");
+
+		VerifyScreenshot();
+	}
+}
+#endif
